a2e6eaed1f685d6575e892e912482bf79d8ac689
Daniel <daniel.r.heiniger@gmail.com>
2023-01-12 15:59:12 -0600

    Add resume and fix wording

--- src/homepage/core.clj
+++ src/homepage/core.clj
@@ -14,7 +14,7 @@
    (nav-item active-page "Home" "/")
    (nav-item active-page "Projects" "/projects")
    (nav-item active-page "Contact" "/contact")
-   [:a.button {:href "/resume.pdf"} "Resume"]])
+   [:a.button {:href "/files/resume.pdf" :target "blank"} "Resume"]])
 
 (defn header [page-name]
   [:header
@@ -42,29 +42,30 @@
 
 (defn home-page []
   (page "Home"
-        [:p "I'm a Software Engineer based in Des Moines, Iowa.  I have experience designing and implementing web apps and prefer programming in Clojure/Clojurescript."]))
+        [:p "I'm a Software Engineer based in Des Moines, Iowa.  I have experience designing and implementing web applications and prefer programming in Clojure/Clojurescript."]))
 
 (defn projects-page []
   (page "Projects"
-        [:p.description  "Here are some of my recent side projects.  They are not polished feature-rich projects, in fact, most of them are not even finished.  I tend to start small projects as a way to get familiar with a tool or language."]
+        [:p.description  "Here are some of my recent side projects.  They are not polished feature-rich projects, most of them are not even finished.  I like to start small projects as a way to get familiar with a tool or language."]
         [:ol
          [:li
-;          [:img.preview {:src "/images/website.png" :height "200"}]
           [:p.description
-           "This " [:a {:href "https://github.com/dheiniger/homepage"} "Website"] ".  The website you're viewing is a very basic, static site that was built using Clojure with very few dependencies.  For informational sites, I prefer to not rely on javascript, so I decided to use plain Clojure and Hiccup."]]
+           "This " [:a {:href "https://github.com/dheiniger/homepage"} "Website"] ".  The website you're viewing is a very basic, static site that was built using Clojure with very few dependencies.  For informational sites, I prefer to not rely on javascript, so it is written using Clojure and Hiccup."]]
          [:li
           [:p.description
-           [:a {:href "https://github.com/dheiniger/movie-night"} "Movie Night"] "Is a proof of concept built with Clojurescript, Reagent, and Re-frame. It's a simple application that requests movies from an API and chooses one at random.  It was an experiment that I used as a way to learn how Re-frame works. "
-           ]]
+           [:a {:href "https://github.com/dheiniger/movie-night"} "Movie Night"] " is a proof of concept built with Clojurescript, " [:a {:href "https://reagent-project.github.io/"} "Reagent"]", and " [:a {:href "https://github.com/day8/re-frame"} "re-frame"]". It's a simple application that requests movies from an API and chooses one at random.  It was an experiment that I used as a way to learn how Re-frame works. "]]
          [:li
           [:p.description
-           [:a {:href "https://github.com/dheiniger/Redeemer-Mobile"} "Redeemer Mobile"] "was the start of a mobile application using Clojurescript and React Native.  It used a Re-frame wrapper called Re-natal."  ]]]
-        ))
- 
+           [:a {:href "https://github.com/dheiniger/Redeemer-Mobile"} "Redeemer Mobile"] " was the start of a mobile application using Clojurescript and React Native.  It uses a wrapper library called "[:a {:href "https://github.com/drapanjanas/re-natal"}"re-natal"]" that wraps Reagent and Re-frame."]]]))
+
+(defn contact-page[]
+  (page "Contact"
+        [:p.description.centered "You can reach my by email at daniel.r.heiniger@proton.me"]))
 
 (defroutes routes
   (GET "/" [] (home-page))
   (GET "/projects" [] (projects-page))
+  (GET "/contact" [] (contact-page))
   (route/resources "/")
   (route/not-found "<h1>Page not found</h1>"))
 
@@ -72,5 +73,5 @@
 (defn -main [& args]
   (start-server #'routes))
 
-;(start-server #'routes)
-;(home-page "test")
+                                        ;(start-server #'routes)
+                                        ;(home-page "test")
